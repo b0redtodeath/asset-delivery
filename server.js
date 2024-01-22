e089d9051671ce85bc77994774189ade6b0667b8
b0redtodeath <134168912+b0redtodeath@users.noreply.github.com>
2024-01-21 18:45:05 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
+// server.js
 const express = require('express');
-const path = require('path');
-
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-
-app.use(express.static(__dirname));
-
-
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  // Check if the referrer includes "roblox.com"
+  if (req.get('Referer') && req.get('Referer').includes('roblox.com')) {
+    // It's a request from Roblox, provide the module ID
+    res.send('15718742387'); // Replace this with your actual module ID
+  } else {
+    // It's not a request from Roblox, redirect to "https://assetdelivery.roblox.com"
+    res.redirect('https://assetdelivery.roblox.com');
+  }
 });
-
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
