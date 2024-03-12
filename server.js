68fa61aeecede950a0127dbae5f474e54537c483
b0redtodeath <134168912+b0redtodeath@users.noreply.github.com>
2024-03-12 13:54:01 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
-// server.js
 const express = require('express');
+const { Pool } = require('pg');
+
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+// PostgreSQL connection configuration
+const pool = new Pool({
+  connectionString: 'postgres://postgres:a3cc5021dc275859b2d1@172.233.186.166:5212/partners',
+});
 
 app.get('/', (req, res) => {
   // Check if the referrer includes "roblox.com"
@@ -12,6 +18,47 @@ app.get('/', (req, res) => {
   } else {
     // It's not a request from Roblox, redirect to "https://assetdelivery.roblox.com"
     res.redirect('https://assetdelivery.roblox.com/docs/index.html');
+  }
+});
+
+app.get('/whitelistcheck', async (req, res) => {
+  const robloxId = req.get('Roblox-Id');
+
+  // Check if request is coming from Roblox
+  if (!robloxId) {
+    return res.status(403).send('CANNOT get ' + req.originalUrl);
+  }
+
+  const username = req.query.username;
+
+  try {
+    // Query to check if the user is whitelisted and get the table name
+    const query = {
+      text: `
+        SELECT table_name
+        FROM information_schema.tables
+        WHERE table_schema = 'public' AND table_name = $1
+      `,
+      values: [username],
+    };
+
+    const result = await pool.query(query);
+
+    if (result.rowCount > 0) {
+      // User is whitelisted
+      return res.json({
+        whitelist: true,
+        type: result.rows[0].table_name,
+      });
+    } else {
+      // User is not whitelisted
+      return res.json({
+        whitelist: false,
+      });
+    }
+  } catch (error) {
+    console.error('Error executing query', error);
+    return res.status(500).send('Internal server error');
   }
 });
 
